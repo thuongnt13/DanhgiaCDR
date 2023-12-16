bd8cbcba527404a374396684f13cd5263e5b10be
nguyễn thương <nguyen13thuong1306@gmail.com>
2023-12-16 11:00:55 +0700

    ok nha

--- DanhgiaCDR/Areas/Admin/Controllers/AdminSVController.cs
+++ DanhgiaCDR/Areas/Admin/Controllers/AdminSVController.cs
@@ -106,7 +106,7 @@ namespace DanhgiaCDR.Areas.Admin.Controllers
                 _context.Update(sv);
                 _context.SaveChanges();
 
-                return Json(new { code = 200, msg = "Cập nhật thành công" });
+                return Json(new { code = 200, msg = "Cập nhật thành công ok" });
 
             }
             catch (Exception ex)
