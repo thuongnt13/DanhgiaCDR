0e336e8d5c348e30941450a21b7532908a7f8ef9
nguyễn thương <nguyen13thuong1306@gmail.com>
2023-12-20 14:30:26 +0700

    update danh gia, danh sach

--- DanhgiaCDR/Controllers/DanhsachController.cs
+++ DanhgiaCDR/Controllers/DanhsachController.cs
@@ -12,33 +12,26 @@ namespace DanhgiaCDR.Controllers
 
         public DanhSachController(dbDanhgiaCDRContext context)
         {
-           _context = context;
+            _context = context;
         }
 
         public ActionResult Index(int MH_ID)
         {
-            var danhSach = (from mh in _context.tblMHs
-                            join sv in _context.tblSVs on mh.MH_ID equals sv.MH_ID
-                            join ctdt in _context.tblCTDTs on mh.CTDT_ID equals ctdt.CTDT_ID
-                            join nganh in _context.tblNganhs on mh.NGANH_ID equals nganh.NGANH_ID
-                            join loaiPhieu in _context.tblLoaiPhieus on mh.MH_ID equals loaiPhieu.MH_ID
-                            where mh.MH_ID == MH_ID
-                            select new View_Danhsach
-                            {
-                                SV_ID = sv.SV_ID,
-                                SV_Ten = sv.SV_Ten,
-                                CTDT_ID = ctdt.CTDT_ID,
-                                CTDT_Ten = ctdt.CTDT_Ten,
-                                NGANH_ID = mh.NGANH_ID,
-                                NGANH_Ten = nganh.NGANH_Ten,
-                                LoaiPhieuDanhGia_ID = loaiPhieu.LoaiPhieuDanhGia_ID,
-                                PhieuDanhGia_Ten = loaiPhieu.PhieuDanhGia_Ten,
-                                MH_ID = mh.MH_ID,
-                                MH_Ten = mh.MH_Ten
-                            })
-                    .ToList();
+            if (string.IsNullOrEmpty(MH_ID.ToString()))
+            {
+                var fullList = _context.tblSVs.OrderBy(d => d.SV_ID).ToList();
+                return View(fullList);
+            }
+            else
+            {
+                Console.WriteLine(MH_ID);
+                var query = _context.view_Danhsach
+                            .Where(item => item.MH_ID == MH_ID).ToList();
+                return View(query);
 
-            return View(danhSach);
+            }
+
+
         }
     }
 }
--- DanhgiaCDR/Models/View_Danhsach.cs
+++ DanhgiaCDR/Models/View_Danhsach.cs
@@ -11,8 +11,8 @@ namespace DanhgiaCDR.Models
         public string? CTDT_Ten { get; set; }
         public int NGANH_ID { get; set; }
         public string? NGANH_Ten { get; set; }
-        public int LoaiPhieuDanhGia_ID { get; set; }
-        public string? PhieuDanhGia_Ten { get; set; }
+        //public int LoaiPhieuDanhGia_ID { get; set; }
+        //public string? PhieuDanhGia_Ten { get; set; }
         public int MH_ID { get; set; }
         public string? MH_Ten { get; set; }
       
